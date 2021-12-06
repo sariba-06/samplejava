5d658e2f0ed240e581af917630b8b1ff21668df6
Sariba Begum <saribabegum@gmail.com>
2021-12-06 12:12:33 +0400

    Changes Made in Greetingsgit status

--- src/main/java/com/example/Greeting.java
+++ src/main/java/com/example/Greeting.java
@@ -3,5 +3,6 @@ package com.example;
 public class Greeting {
     public static void sayHi() {
         System.out.println("Hi Bazel Build Test!");
+	   System.out.println("This is newly added.To test the build");
     }
 }
