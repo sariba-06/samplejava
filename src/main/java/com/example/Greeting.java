7fbd2b067fd35b2fdb9af17257e2c5a7f77585ad
Sariba Begum <saribabegum@gmail.com>
2021-12-06 12:17:48 +0400

    Second Change done for greeting

--- src/main/java/com/example/Greeting.java
+++ src/main/java/com/example/Greeting.java
@@ -3,6 +3,8 @@ package com.example;
 public class Greeting {
     public static void sayHi() {
         System.out.println("Hi Bazel Build Test!");
-	   System.out.println("This is newly added.To test the build");
+	    System.out.println("This is newly added.To test the build");
+        System.out.println("Atempt to new build with new change");
+
     }
 }
